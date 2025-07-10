0a66d92d53f7f08e9495aae501ba366cdad21c34
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-07-10 21:04:35 +0530

    feat: implement createToken function for JWT generation

---
+++ src/tokens/createToken.ts
@@ -0,0 +1,6 @@
+import jwt from "jsonwebtoken";
+import { authConfig } from "../config";
+
+export function createToken(payload: any) {
+    return jwt.sign(payload, authConfig.jwtSecret, { expiresIn: "7d" });
+}

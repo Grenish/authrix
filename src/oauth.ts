a8bd64022fbb304fe60de3446f15bb0321da15eb
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-07-11 18:50:28 +0530

    feat: add OAuth providers export for optional imports

---
+++ src/oauth.ts
@@ -0,0 +1,5 @@
+// OAuth providers - separate export for optional imports
+// Import these directly when you need OAuth functionality
+
+export * from "./providers/google";
+export * from "./providers/github";
